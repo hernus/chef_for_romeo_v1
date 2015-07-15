4b6c71e464da86f57f238bc6213914b146c7b848
Hernus Carelsen <hernus@carelsen.com>
2015-07-16 09:37:50 +1000

    first push

---
+++ cookbooks/apt/recipes/update.rb
@@ -0,0 +1,7 @@
+bash 'apt-get update' do
+  cwd "/"
+  user "root"
+  code "apt-get update"
+end
+
+
---
+++ solo.rb
@@ -0,0 +1,6 @@
+cookbook_path '/vagrant/chef2/cookbooks'
+data_bag_path '/vagrant/chef2/data_bags'
+role_path     '/vagrant/chef2/roles'
+
+log_level     :info
+log_location  STDOUT
