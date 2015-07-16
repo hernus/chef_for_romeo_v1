c927e49213f0ca4c4c35e241d50eb05a9daae44f
Hernus Carelsen <hernus@carelsen.com>
2015-07-16 11:15:24 +1000

    fix paths

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
-cookbook_path '/vagrant/chef2/cookbooks'
-data_bag_path '/vagrant/chef2/data_bags'
-role_path     '/vagrant/chef2/roles'
+cookbook_path '/sites/chef/cookbooks'
+data_bag_path '/sites/chef/data_bags'
+role_path     '/sites/chef/roles'
 
 log_level     :info
 log_location  STDOUT
