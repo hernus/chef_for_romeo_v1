1b4b1b7a3751e084a1be161adc4033106c3befaf
Hernus Carelsen <hernus@carelsen.com>
2015-07-16 13:55:31 +1000

    asdf

--- cookbooks/znginx_passenger/recipes/default.rb
+++ cookbooks/znginx_passenger/recipes/default.rb
@@ -21,12 +21,12 @@ package 'nginx-extras'
 
 package 'passenger'
 
-line1 = "passenger_root /usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini;"
-line2 = "passenger_ruby /usr/local/rvm/wrappers/default/ruby;"
+# line1 = "passenger_root /usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini;"
+# line2 = "passenger_ruby /usr/local/rvm/wrappers/default/ruby;"
 
-file = Chef::Util::FileEdit.new('/etc/nginx/nginx.conf')
-file.search_file_replace(/^\s*#\s*passenger_root/, line1)
-file.search_file_replace(/^\s*#\s*passenger_ruby/, line2)
+# file = Chef::Util::FileEdit.new('/etc/nginx/nginx.conf')
+# file.search_file_replace(/^\s*#\s*passenger_root/, line1)
+# file.search_file_replace(/^\s*#\s*passenger_ruby/, line2)
 
 
 file "/etc/nginx/sites-available/romeo" do
